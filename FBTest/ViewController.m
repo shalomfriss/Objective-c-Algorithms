9ad4614f8d168b05c60a40273bf6f6573738b82b
SFRIS1 <shalom.friss@monsanto.com>
2016-07-15 07:27:28 -0500

    Initial Commit

---
+++ FBTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FBTest
+//
+//  Created by FRISS, SHALOM [AG-Contractor/1005] on 7/15/16.
+//  Copyright © 2016 FRISS, SHALOM [AG-Contractor/1005]. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ FBTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FBTest
+//
+//  Created by FRISS, SHALOM [AG-Contractor/1005] on 7/15/16.
+//  Copyright © 2016 FRISS, SHALOM [AG-Contractor/1005]. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
